5c2cbfab899c9f99594d5ef635f69af41a0a55e0
vikash032002 <vikuvikash03@gmail.com>
2025-04-20 11:30:39 +0530

    basic chat setup done

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ const io=socketio(server);
 
 app.use('/',express.static(__dirname+'/public'));
 
+io.on('connect',(socket)=>{
+    console.log("new user connected",socket.id);
+
+    socket.on('msg_send',(data)=>{
+        console.log(data)
+        io.emit('msg_rcv',data);
+    })
+
+    // setInterval(()=>{
+    //     socket.emit('from_server');
+    // },2000)
+});
+
 server.listen(3000,()=>{
     console.log("server started");
-})
+});
